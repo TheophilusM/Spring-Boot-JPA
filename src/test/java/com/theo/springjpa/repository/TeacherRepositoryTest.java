d819741963cdb1f5e493fc042535e8bdd99e2aa2
TheophilusM <theotakumug@gmail.com>
2022-04-18 23:05:07 +0200

    teacher repo test by creating teacher with courses

---
+++ src/test/java/com/theo/springjpa/repository/TeacherRepositoryTest.java
@@ -0,0 +1,53 @@
+package com.theo.springjpa.repository;
+
+import com.theo.springjpa.entity.Course;
+import com.theo.springjpa.entity.Teacher;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class TeacherRepositoryTest {
+
+    @Autowired
+    private TeacherRepository teacherRepository;
+
+    @Test
+    public void saveTeacher() {
+        // can create a method to add a course to the course lists
+        Course course1 = Course
+                .builder()
+                .title("Maths")
+                .credit(10)
+                .build();
+        Course course2 = Course
+                .builder()
+                .title("English")
+                .credit(10)
+                .build();
+            Course course3 = Course
+                .builder()
+                .title("Computers")
+                .credit(10)
+                .build();
+            Course course4 = Course
+                .builder()
+                .title("Physics")
+                .credit(10)
+                .build();
+
+        Teacher teacher = Teacher
+                .builder()
+                .firstName("John")
+                .lastName("Doe")
+                .courses(List.of(course1,course2,course3,course4))
+                .build();
+
+        teacherRepository.save(teacher);
+    }
+
+}
