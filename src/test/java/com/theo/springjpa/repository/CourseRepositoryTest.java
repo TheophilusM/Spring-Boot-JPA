1da97adfb4b16697d72e31cc0b37216ae221f44f
TheophilusM <theotakumug@gmail.com>
2022-04-18 23:28:53 +0200

    test teacher entity

--- src/test/java/com/theo/springjpa/repository/CourseRepositoryTest.java
+++ src/test/java/com/theo/springjpa/repository/CourseRepositoryTest.java
@@ -1,11 +1,12 @@
 package com.theo.springjpa.repository;
 
 import com.theo.springjpa.entity.Course;
-import com.theo.springjpa.entity.CourseMaterial;
 import com.theo.springjpa.entity.Teacher;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -38,5 +39,14 @@ class CourseRepositoryTest {
                         .build();
 
         courseRepository.save(course);
+    }
+
+    @Test
+    public void findAllPagination() {
+        Pageable pageable3 = PageRequest.of(0,3);
+        Pageable pageable2 = PageRequest.of(1,2);
+
+        List<Course> courses = courseRepository.findAll(pageable3).getContent();
+        System.out.println(courses);
     }
 }
